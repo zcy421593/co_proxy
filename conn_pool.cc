36c1caed2d8b8a302252fc578d78e458e2869d8b
zcy <zcy421593@126.com>
2015-12-15 12:37:20 +0800

    connection pool fix

--- conn_pool.cc
+++ conn_pool.cc
@@ -5,48 +5,72 @@
 #include <vector>
 #include <map>
 #include <unistd.h>
+#include <stdlib.h>
+#include "list.h"
 using namespace std;
+
+struct fd_info {
+	event* ev;
+	list_head list;
+	int fd;
+};
 
 struct conn_info {
 	string tag;
-	int fd;
-	event* ev;
+	list_head list_fd;
+	
 };
 static map<string, conn_info*> s_map;
 
 static void event_cb(int fd, short what, void* args) {
-	conn_info* info = (conn_info*)args;
-	if(s_map.find(info->tag) != s_map.end()) {
-		s_map.erase(info->tag);
-	}
-	
+	fd_info* info = (fd_info*)args;
+
+	list_del(&info->list);
 	event_free(info->ev);
 	close(info->fd);
-	delete info;
+	free(info);
 } 
 
 void pool_queue_connection(event_base* base, std::string dest, int port, int fd) {
 	string tag = get_format_string("%s:%d", dest.c_str(), port);
-	conn_info* conn = new conn_info;
-	conn->fd = fd;
-	conn->tag = tag;
+	conn_info* conn = NULL;
+	fd_info* info = (fd_info*)calloc(1, sizeof(fd_info));
+	info->fd = fd;
+	info->ev = event_new(base, fd, EV_READ, NULL, conn);
 	if(s_map.find(tag) == s_map.end()) {
-		s_map[tag] = conn;
-		conn->ev = event_new(base, fd, EV_READ, NULL, conn);
+		conn = new conn_info;
+		conn->tag = tag;
+		conn->list_fd.next = &conn->list_fd;
+		conn->list_fd.prev = &conn->list_fd;
+		s_map[tag] = conn;		
 	} else {
-		close(fd);
+		conn = s_map[tag];
 	}
+	list_add(&info->list, &conn->list_fd);
 }
+
 int  pool_get_connection(std::string dest, int port) {
 	string tag = get_format_string("%s:%d", dest.c_str(), port);
+	int ret = -1;
 	if(s_map.find(tag) != s_map.end()) {
-		conn_info* info = s_map[tag];
-		event_free(info->ev);
-		int tmp = info->fd;
-		s_map.erase(info->tag);
-		delete info;
+		conn_info* conn = s_map[tag];
+		if(!list_empty(&conn->list_fd)) {
 
-		return tmp;
+			fd_info* info;
+			fd_info* tmp;
+			list_for_each_entry_safe(info, tmp, &conn->list_fd, list) {
+				ret = info->fd;
+				list_del(&info->list);
+				event_del(info->ev);
+				event_free(info->ev);
+				free(info);
+				break;
+			}
+			
+		} else {
+			s_map.erase(tag);
+			delete conn;
+		}
 	}
-	return -1;
+	return ret;
 }
