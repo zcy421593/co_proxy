6dc289314001b453ab3f740ae743cc1b70c31bd8
zcy <zcy421593@126.com>
2016-01-31 09:48:31 +0800

    seperate read write taskid

--- co_socket.cc
+++ co_socket.cc
@@ -17,7 +17,8 @@ struct co_socket {
 	list_head list;
 	int fd;
 	co_base* base;
-	int task_id;
+	int read_task_id;
+	int write_task_id;
 	event* event_read;
 	event* event_write;
 	event* event_cancel;
@@ -32,7 +33,14 @@ struct co_socket {
 static void cancelcb(int fd, short what, void* args) {
 	co_socket* sock = (co_socket*)args;
 	sock->is_task_canceled = true;
-	coroutine_resume(sock->base->sch, sock->task_id);
+
+	if(sock->read_task_id != -1) {
+		coroutine_resume(sock->base->sch, sock->read_task_id);
+	}
+	if(sock->write_task_id != -1) {
+		coroutine_resume(sock->base->sch, sock->write_task_id);
+	}
+	
 }
 
 void co_socket_error_all() {
@@ -54,7 +62,8 @@ struct co_socket* co_socket_create(co_base* base) {
 	sock->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	sock->read_timeout = -1;
 	sock->write_timeout = -1;
-	sock->task_id = -1;
+	sock->read_task_id = -1;
+	sock->write_task_id = -1;
 	evutil_make_socket_nonblocking(sock->fd);
 	list_add(&sock->list, &s_head);
 	return sock;
@@ -66,7 +75,8 @@ struct co_socket* co_socket_create_with_fd(co_base* base, int fd) {
 	sock->fd = fd;
 	sock->read_timeout = -1;
 	sock->write_timeout = -1;
-	sock->task_id = -1;
+	sock->write_task_id = -1;
+	sock->read_task_id = -1;
 	evutil_make_socket_nonblocking(sock->fd);
 	list_add(&sock->list, &s_head);
 	return sock;
@@ -95,13 +105,16 @@ int co_socket_listen(co_socket* sock, int backlog) {
 static void acceptcb(int fd, short what, void* args) {
 
 	co_socket* sock = (co_socket*)args;
-	int tmp = sock->task_id;
+	int tmp = sock->read_task_id;
+	//sock->read_task_id = -1;
 	coroutine_resume(sock->base->sch, tmp);
 }
 
 static void conncb(int fd, short what, void* args) {
 	co_socket* sock = (co_socket*)args;
-	coroutine_resume(sock->base->sch, sock->task_id);
+	int tmp = sock->write_task_id;
+	//sock->write_task_id = -1;
+	coroutine_resume(sock->base->sch, tmp);
 }
 
 static void writecb(int fd, short what, void* args) {
@@ -110,7 +123,9 @@ static void writecb(int fd, short what, void* args) {
 		printf("write timeout\n");
 		sock->is_write_timeout = true;
 	}
-	coroutine_resume(sock->base->sch, sock->task_id);
+	int tmp = sock->write_task_id;
+	//sock->write_task_id = -1;
+	coroutine_resume(sock->base->sch, tmp);
 }
 
 static void readcb(int fd, short what, void* args) {
@@ -122,7 +137,9 @@ static void readcb(int fd, short what, void* args) {
 		printf("read timeout\n");
 		sock->is_read_timeout = true;
 	}
-	coroutine_resume(sock->base->sch, sock->task_id);
+	int tmp = sock->read_task_id;
+	//sock->read_task_id = -1;
+	coroutine_resume(sock->base->sch, tmp);
 }
 
 void co_socket_set_connecttimeout(co_socket* sock, int ms) {
@@ -157,7 +174,7 @@ int co_socket_connect(co_socket* sock, const char* sz_addr, int port) {
 	}
 
 	sock->event_write = event_new(sock->base->base, sock->fd, EV_WRITE, conncb, sock);
-	sock->task_id = coroutine_running(sock->base->sch);
+	sock->write_task_id = coroutine_running(sock->base->sch);
 	event_add(sock->event_write, sock->write_timeout == -1 ? NULL : &val);
 	coroutine_yield(sock->base->sch);
 
@@ -179,7 +196,7 @@ co_socket* co_socket_accept(co_socket* sock) {
 	if(!sock->event_read) {
 		sock->event_read = event_new(sock->base->base, sock->fd, EV_READ, acceptcb, sock);
 	}	
-	sock->task_id = coroutine_running(sock->base->sch);
+	sock->read_task_id = coroutine_running(sock->base->sch);
 	event_add(sock->event_read, NULL);
 
 	coroutine_yield(sock->base->sch);
@@ -219,7 +236,7 @@ int co_socket_read_peek(co_socket* sock, char* buf, int len) {
 		sock->event_read = event_new(sock->base->base, sock->fd, EV_READ, readcb, sock);
 	}
 
-	sock->task_id = coroutine_running(sock->base->sch);
+	sock->read_task_id = coroutine_running(sock->base->sch);
 	event_add(sock->event_read, NULL);
 	coroutine_yield(sock->base->sch);
 
@@ -267,7 +284,7 @@ int co_socket_read(co_socket* sock, char* buf, int len) {
 		sock->event_read = event_new(sock->base->base, sock->fd, EV_READ, readcb, sock);
 	}
 
-	sock->task_id = coroutine_running(sock->base->sch);
+	sock->read_task_id = coroutine_running(sock->base->sch);
 	event_add(sock->event_read, sock->read_timeout == -1 ? NULL : &val);
 	coroutine_yield(sock->base->sch);
 
@@ -325,7 +342,7 @@ int co_socket_readline(co_socket* sock, char* buf, int len) {
 		sock->event_read = event_new(sock->base->base, sock->fd, EV_READ, readcb, sock);
 	}
 
-	sock->task_id = coroutine_running(sock->base->sch);
+	sock->read_task_id = coroutine_running(sock->base->sch);
 
 	for(;;) {
 		event_add(sock->event_read,  sock->read_timeout == -1 ? NULL : &val);
@@ -403,7 +420,7 @@ int co_socket_write(co_socket* sock, char* buf, int len) {
 					sock->event_write = event_new(sock->base->base, sock->fd, EV_WRITE, writecb, sock);
 				}
 				event_add(sock->event_write, NULL);
-				sock->task_id = coroutine_running(sock->base->sch);
+				sock->write_task_id = coroutine_running(sock->base->sch);
 				coroutine_yield(sock->base->sch);
 
 				if(sock->is_task_canceled) {
@@ -429,7 +446,7 @@ int co_socket_write(co_socket* sock, char* buf, int len) {
 			if(!sock->event_write) {
 				sock->event_write = event_new(sock->base->base, sock->fd, EV_WRITE, acceptcb, sock);
 			}
-			sock->task_id = coroutine_running(sock->base->sch);
+			sock->write_task_id = coroutine_running(sock->base->sch);
 			event_add(sock->event_write, NULL);
 			printf("write coroutine_yield");
 			coroutine_yield(sock->base->sch);
